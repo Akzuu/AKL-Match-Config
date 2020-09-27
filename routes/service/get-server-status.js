0b3397602892e7fb3de072bdb697612aa44bf81c
Akseli Kolari <akseli.kolari@tuni.fi>
2020-09-27 18:54:33 +0300

    server status

---
+++ routes/service/get-server-status.js
@@ -0,0 +1,58 @@
+const { log } = require('../../lib');
+const { MatchConfig } = require('../../models');
+
+const schema = {
+  description: 'Check if server has match going on (by schedule)',
+  summary: 'Check if there is match going on',
+  tags: ['Service'],
+  params: {
+    type: 'object',
+    properties: {
+      server: {
+        type: 'string',
+      },
+    },
+  },
+};
+
+const handler = async (req, reply) => {
+  const currentTime = new Date();
+  const currentTimePlusFifteen = new Date();
+  currentTimePlusFifteen.setMinutes(currentTimePlusFifteen.getMinutes + 15);
+
+  let matchConfig;
+  try {
+    matchConfig = await MatchConfig.findOne({
+      server: req.params.server,
+      'matchDate.startTime': {
+        $gte: currentTimePlusFifteen,
+      },
+      'matchDate.endTime': {
+        $lte: currentTime,
+      },
+    });
+  } catch (error) {
+    log.error('Error when trying to find config! ', error);
+    reply.status(500).send({
+      status: 'ERROR',
+      error: 'Internal Server Error',
+    });
+    return;
+  }
+
+  if (!matchConfig) {
+    reply.send(false);
+    return;
+  }
+
+  reply.send(true);
+};
+
+module.exports = async function (fastify) {
+  fastify.route({
+    method: 'GET',
+    url: '/config/:server',
+    handler,
+    schema,
+  });
+};
--- routes/service/index.js
+++ routes/service/index.js
@@ -1,7 +1,9 @@
 const getConfig = require('./get-config');
 const receiveConfig = require('./receive-config');
+const getServerStatus = require('./get-server-status');
 
 module.exports = {
   getConfig,
   receiveConfig,
+  getServerStatus,
 };
